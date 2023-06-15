972740321854e58007c61da62a1e9e521d91086c
weigp1 <2524634086@qq.com>
2023-06-15 21:44:00 +0800

    实现图片上传、下载

--- src/main/java/com/example/demo/repository/AttentionRepository.java
+++ src/main/java/com/example/demo/repository/AttentionRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.Attention;
+import com.example.demo.entity.Attention;
 
 @Repository
 public interface AttentionRepository extends JpaRepository<Attention, Integer> {
--- src/main/java/com/example/demo/service/UserController.java
+++ src/main/java/com/example/demo/service/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.User;
+import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.google.gson.Gson;
 
